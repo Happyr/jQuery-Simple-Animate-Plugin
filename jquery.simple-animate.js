72d2ed7e6c92149b1210ecbbbbf877589d0142c6
Nyholm <tobias.nyholm@gmail.com>
2013-07-08 15:57:15 +0200

    Init

---
+++ jquery.simple-animate.js
@@ -0,0 +1,43 @@
+/*!
+ * jQuery Simple Animate Plugin
+ * version: 1.1.3
+ * @requires jQuery v1.5 or later
+ * Copyright (c) 2013 Tobias Nyholm
+ *
+ * Project repository: https://github.com/HappyR/jQuery-Simple-Animate-Plugin
+ * Licensed under the MIT
+ */
+
+/**
+ * This is just aliases to animiate show and hide funcions
+ */
+(function($) {
+	  $.fn.animateShow = function() {
+	    return this.each(function() {
+	    	$(this).animate({ height: 'show', opacity: 'show' }, 'slow');
+	    });
+	  };
+	  
+	  $.fn.animateHide = function() {
+		 return this.each(function() {
+		    $(this).animate({ height: 'hide', opacity: 'hide' }, 'slow');
+		 });
+	  };
+
+      $.fn.animateToggle = function() {
+          return this.each(function() {
+              if($(this).is(":visible"))
+                  $(this).animateHide();
+              else
+                  $(this).animateShow();
+          });
+      };
+	  
+	  $.fn.animateRemove = function() {
+			 return this.each(function() {
+			    $(this).animate({ height: 'hide', opacity: 'hide' }, 'slow', 'linear', function() {
+				      $(this).remove();
+				  });
+			 });
+		  };
+	})(jQuery);
