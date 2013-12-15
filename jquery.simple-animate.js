e94856002c2cc6b6b79a5675aefc69617efcc722
Nyholm <tobias.nyholm@gmail.com>
2013-12-15 11:53:35 +0100

    Reformat

--- jquery.simple-animate.js
+++ jquery.simple-animate.js
@@ -11,33 +11,33 @@
 /**
  * This is just aliases to animiate show and hide funcions
  */
-(function($) {
-	  $.fn.animateShow = function() {
-	    return this.each(function() {
-	    	$(this).animate({ height: 'show', opacity: 'show' }, 'slow');
-	    });
-	  };
-	  
-	  $.fn.animateHide = function() {
-		 return this.each(function() {
-		    $(this).animate({ height: 'hide', opacity: 'hide' }, 'slow');
-		 });
-	  };
+(function ($) {
+    $.fn.animateShow = function () {
+        return this.each(function () {
+            $(this).animate({ height: 'show', opacity: 'show' }, 'slow');
+        });
+    };
 
-      $.fn.animateToggle = function() {
-          return this.each(function() {
-              if($(this).is(":visible"))
-                  $(this).animateHide();
-              else
-                  $(this).animateShow();
-          });
-      };
-	  
-	  $.fn.animateRemove = function() {
-			 return this.each(function() {
-			    $(this).animate({ height: 'hide', opacity: 'hide' }, 'slow', 'linear', function() {
-				      $(this).remove();
-				  });
-			 });
-		  };
-	})(jQuery);
+    $.fn.animateHide = function () {
+        return this.each(function () {
+            $(this).animate({ height: 'hide', opacity: 'hide' }, 'slow');
+        });
+    };
+
+    $.fn.animateToggle = function () {
+        return this.each(function () {
+            if ($(this).is(":visible"))
+                $(this).animateHide();
+            else
+                $(this).animateShow();
+        });
+    };
+
+    $.fn.animateRemove = function () {
+        return this.each(function () {
+            $(this).animate({ height: 'hide', opacity: 'hide' }, 'slow', 'linear', function () {
+                $(this).remove();
+            });
+        });
+    };
+})(jQuery);
